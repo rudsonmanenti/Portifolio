f8b04c0f0bf27818f62cc0443b84be26fa6e1d5a
Rudson Manenti Valim <rudsonmanenti3@gmail.com>
2026-03-20 05:12:00 -0300

    adicionado menu hamb

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.addEventListener("scroll", () => {
-
 const header = document.querySelector(".header")
-
 if(window.scrollY > 50){
-
 header.style.background = "rgba(0,0,0,0.9)"
-
 }
-
+})
+const toggle = document.getElementById("menu-toggle")
+const menu = document.getElementById("menu")
+toggle.addEventListener("click", () => {
+menu.classList.toggle("active")
 })
